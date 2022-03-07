9dde75b731f198467adf30331d92fe44a336055c
bajerj.03 <bajerj.03@A321-12>
2022-03-07 12:14:26 +0100

    Přidejte soubory projektu.

---
+++ Metody09-14-Github/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metody09_14_Github
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = double.Parse(textBox3.Text);
+            double x1, x2, diskriminant;
+            Swag.Diskriminant(a, b, c, out x1, out x2, out diskriminant);
+            if (diskriminant < 0) MessageBox.Show("Rovnice nemá řešení v R");
+            else if (diskriminant > 0) MessageBox.Show("Rovnice má 2 kořeny");
+            else MessageBox.Show("Rovnice má jeden dvojnásobný kořen " + x1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string retezec = textBox4.Text;
+            int cifSoucet = 0, soucetLichCif = 0, soucetSudCif = 0;
+            if (Swag.ObsahujeCislici(retezec, out cifSoucet, out soucetLichCif, out soucetSudCif))
+            {
+                MessageBox.Show("Obsahuje číslici");
+                MessageBox.Show("Ciferný součet: " + cifSoucet);
+                MessageBox.Show("Ciferný součet lichých čísel: " + soucetLichCif);
+                MessageBox.Show("Ciferný součet sudých čísel: " + soucetSudCif);
+            }
+        }
+    }
+}
---
+++ Metody09-14-Github/Swag.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metody09_14_Github
+{
+    class Swag
+    {
+        public static double Diskriminant(double a, double b, double c, out double x1, out double x2, out double diskriminant)
+        {
+            diskriminant = (b * b) - (4 * a * c);
+            x1 = 0;
+            x2 = 0;
+            if (diskriminant < 0)
+            {
+                
+                x1 = (-b + Math.Sqrt(diskriminant)) / (2 * a);
+                x2 = (-b - Math.Sqrt(diskriminant)) / (2 * a);
+            }
+            else if (diskriminant > 0)
+            {
+                diskriminant = 0;
+            }
+            else
+            {
+                x1 = (-b + Math.Sqrt(diskriminant)) / (2 * a);
+            }
+            return diskriminant;
+        }
+
+
+        public static bool ObsahujeCislici(string retezec, out int cifSoucet, out int soucetLichCif, out int soucetSudCif)
+        {
+            bool obsahujeCislici = false;
+            cifSoucet = 0;
+            soucetLichCif = 0;
+            soucetSudCif = 0;
+            for (int i = 0; i < retezec.Length; ++i)
+            {
+                if (Char.IsDigit(retezec[i]))
+                {
+                    obsahujeCislici = true;
+                    cifSoucet += int.Parse(retezec[i].ToString());
+
+                    if (int.Parse(retezec[i].ToString()) % 2 == 0)
+                    {
+                        soucetSudCif += int.Parse(retezec[i].ToString());
+                    }
+                    else if (int.Parse(retezec[i].ToString()) % 2 != 0)
+                    {
+                        soucetLichCif += int.Parse(retezec[i].ToString());
+                    }
+                }
+            }
+
+            return obsahujeCislici;
+        }
+    }
+}
